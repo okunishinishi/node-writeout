9e04954f6b8b3f54644cbb9705990062114dafac
Taka Okunishi <okunishitaka.com@gmail.com>
2016-05-19 14:32:09 +0900

    Version incremented to 2.0.4

--- example/example-writeout.js
+++ example/example-writeout.js
@@ -7,11 +7,10 @@ writeout('hello-wold', 'This is the contents text', {
   mkdirp: true,
   skipIfIdentical: true
 }).then((result) => {
-  if (err) {
-    console.error(err);
-  } else {
-    if (!result.skipped) {
-      console.log('File generated:', result.filename);
-    }
+  if (!result.skipped) {
+    console.log('File generated:', result.filename)
   }
-})
+}).catch((err) =>
+  console.error(err)
+)
+
