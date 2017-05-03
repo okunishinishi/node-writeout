e2d276775eb1861f14cad5c1b32238d82617163b
Taka Okunishi <okunishinishi@gmail.com>
2017-05-03 16:49:07 +0900

    Version incremented to 2.2.1

---
+++ test/helpers_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for helpers.
+ * Runs with mocha.
+ */
+'use strict'
+
+const helpers = require('../lib/helpers.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('helpers', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Helpers', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
